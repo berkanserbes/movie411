8f60ee9246efd8195adb4c7efa36ea18156b0565
Berkan Serbes <berkan_serbes@hotmail.com>
2023-06-29 12:33:30 +0300

    Modified about page

---
+++ src/app/about/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4 p-4">
+      <h1 className="text-2xl text-amber-600">About Us</h1>
+      <p>About Us text...</p> {/* will be filled later */}
+    </div>
+  );
+};
+
+export default About;
