d9a3eae8814e5ce076575be80236eb54e11cffbe
Berkan Serbes <berkan_serbes@hotmail.com>
2023-06-29 12:36:27 +0300

    Modified NavbarItem.jsx

---
+++ src/components/NavbarItem.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useSearchParams } from "next/navigation";
+import Link from "next/link";
+
+const NavbarItem = ({ title, param }) => {
+  const searchParams = useSearchParams();
+  const genre = searchParams.get("genre");
+  console.log(genre);
+  return (
+    <div>
+      <Link
+        href={`/?genre=${param}`}
+        className={`hover:text-gray-700 p-2 ${
+          genre === param &&
+          "underline underline-offset-8 decoration-2 decoration-amber-600 text-amber-800 rounded-lg"
+        }`}
+      >
+        {title}
+      </Link>
+    </div>
+  );
+};
+
+export default NavbarItem;
