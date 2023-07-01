0334dc9a3721658dd22b727f986baae1bbd70507
Berkan Serbes <berkan_serbes@hotmail.com>
2023-07-01 18:26:04 +0300

    Modified files

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import { AiFillHome, AiFillInfoCircle } from "react-icons/ai";
 
 const Header = () => {
   return (
-    <div className="flex justify-between items-center py-6 w-screen bg-red-400">
+    <div className="flex justify-between items-center py-6 w-screen bg-radial-gradient">
       <div className="flex">
         <MenuItem title={"Home"} address={"/"} Icon={AiFillHome} />
         <MenuItem title={"About"} address={"/about"} Icon={AiFillInfoCircle} />
@@ -13,7 +13,9 @@ const Header = () => {
 
       <div className="flex items-center space-x-5">
         <Link href="/">
-          <h2 className="text-2xl pe-3 lg:pe-16">Movie411</h2>
+          <h2 className="text-2xl lg:text-3xl pe-3 lg:pe-16 text-yellow-600">
+            Movie411
+          </h2>
         </Link>
       </div>
     </div>
--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -5,7 +5,9 @@ const MenuItem = ({ title, address, Icon }) => {
   return (
     <Link href={address} className="mx-4 lg:mx-6 hover:text-amber-600">
       <Icon className="text-2xl sm:hidden mx-2" />
-      <span className="hidden sm:inline my-2 text-sm">{title}</span>
+      <span className="hidden sm:inline my-2 text-sm lg:text-lg text-red-500">
+        {title}
+      </span>
     </Link>
   );
 };
