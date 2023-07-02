44c94dd507ca2e83b43286c39fb71f84edf6ba82
Berkan Serbes <berkan_serbes@hotmail.com>
2023-07-02 16:27:20 +0300

    Modified About Us page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -3,8 +3,27 @@ import React from "react";
 const About = () => {
   return (
     <div className="max-w-6xl mx-auto space-y-4 p-4">
-      <h1 className="text-2xl text-amber-600">About Us</h1>
-      <p>About Us text...</p> {/* will be filled later */}
+      <h1 className="text-2xl text-red-500">About Us</h1>
+      <p>
+        Movie 411 is a film platform created for cinema lovers to discover the
+        latest trends and experience the most acclaimed films. <br /> <br /> In
+        our Trending section, you can find the most talked about films that have
+        gained popularity in the last week. This section is an excellent
+        resource for users who want to follow current trends in the film world.
+        With content renewed every week, you can stay up to date with new and
+        exciting films. <br /> <br /> Our top rated section brings together the
+        most acclaimed films of all time. This exclusive collection includes
+        masterpieces that have left their mark on the history of cinema and
+        works worth watching. The films in this section are highly rated by
+        critics and users. It will be of interest to anyone who wants to
+        discover and watch important productions in the history of cinema.
+        <br /> <br /> When you want to know more about the film in Movie 411,
+        just click on the film picture. Thus, you can get a better idea about
+        the film and make your viewing decision in a more informed way.
+        <br /> <br />
+        Welcome to the fascinating journey of the film world. Enjoy a full
+        cinema experience every moment with Movie 411
+      </p>
     </div>
   );
 };
